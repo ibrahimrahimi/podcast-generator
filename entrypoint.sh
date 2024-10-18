7186ce97566e9054bb7fb61093aa30b86e80a3a5
Ibrahim Rahimi <38061603+ibrahimrahimi@users.noreply.github.com>
2024-10-18 15:28:51 +0000

    Update actions.yml file.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,18 @@
 
 echo "---------------------------------------------"
 
-git config --global user.name "${GITHUB_ACTOR}"
-git config --global user.email "${INPUT_EMAIL}"
+# Set the default values from inputs or environment variables
+GITHUB_NAME="${INPUT_NAME:-$GITHUB_ACTOR}"
+GITHUB_EMAIL="${INPUT_EMAIL:-$GITHUB_ACTOR@users.noreply.github.com}"
+
+git config --global user.name "$GITHUB_NAME"
+git config --global user.email "$GITHUB_EMAIL"
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
-git add -A && git commit -m "Update Feed""
+git add -A && git commit -m "Update Feed"
 
 git push --set-upstream origin main
-
 
 echo "---------------------------------------------"
